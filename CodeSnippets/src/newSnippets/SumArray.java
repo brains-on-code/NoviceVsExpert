3e799a1eb0baff9e081f303e0bb460fde5e591f1
Jonas Mucke <jonas.mucke@web.de>
2021-04-21 13:22:37 +0200

    updated SumArray

--- CodeSnippets/src/newSnippets/SumArray.java
+++ CodeSnippets/src/newSnippets/SumArray.java
@@ -1,4 +1,19 @@
 package newSnippets;
 
 public class SumArray {
+
+    public static void main(String[] args) {
+        int[] array = { 1, 6, 4, 10, 2 };
+        System.out.println(sumArray(array));
+    }
+
+    public static int sumArray(int[] array){
+        int result = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            result = result + array[i];
+        }
+
+        return result;
+    }
 }
