6bfb838dfb01ee1a95fa90a2b3d8f48f30955616
Jonas Mucke <jonas.mucke@web.de>
2021-05-03 21:21:01 +0200

    updated psychopy experiment

--- Study/test_lastrun.py
+++ Study/test_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.10),
-    on April 20, 2021, at 11:03
+    on Mai 03, 2021, at 15:31
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
