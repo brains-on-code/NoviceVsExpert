d484b27a8d1d565833ac11829a26fef5a0b5e869
Annabelle0598 <44641021+Annabelle0598@users.noreply.github.com>
2021-05-05 12:33:40 +0200

    Update recPower.java

--- CodeSnippets/src/recPower.java
+++ CodeSnippets/src/recPower.java
@@ -2,7 +2,7 @@
  * How much do you like the snippet in a scale from (1(bad) to 5(good)), may leave your comments below
  *
  * Norman:
- * Anabell:
+ * Anabelle: 3 very easy, the standard example of recursive in the first programming lecture
  * Jonas
  *
  */
