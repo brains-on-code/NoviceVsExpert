e0e23269917bf1efe780b38a184e47f9ebc2e10b
Annabelle0598 <44641021+Annabelle0598@users.noreply.github.com>
2021-05-05 14:26:54 +0200

    Update PermuteString.java

--- CodeSnippets/src/totallyNewSnippets/PermuteString.java
+++ CodeSnippets/src/totallyNewSnippets/PermuteString.java
@@ -8,7 +8,7 @@ import java.util.List;
  * How much do you like the snippet in a scale from (1(bad) to 5(good)), may leave your comments below
  *
  * Norman:
- * Anabell:
+ * Annabelle: 5
  * Jonas
  *
  */
